e44f8460aface522ce6bc3e2c0d76683f49f56a7
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-17 15:00:44 -0300

    first commit

---
+++ src/test/java/com/tidz/crudStuff/CrudStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.crudStuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
