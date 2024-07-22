5a1704c864415a3265f520adc7ceb6c4c3cfd28c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-22 16:24:04 -0300

    implement new DAO method findCourseAndReviewsByCourseId

--- src/main/java/com/tidz/crudStuff/dao/AppDAOImpl.java
+++ src/main/java/com/tidz/crudStuff/dao/AppDAOImpl.java
@@ -107,4 +107,12 @@ public class AppDAOImpl implements AppDAO {
 	public void save(Course course) {
 		this.entityManager.persist(course);
 	}
+
+	@Override
+	public Course findCourseAndReviewsByCourseId(int id) {
+		TypedQuery<Course> query = this.entityManager
+				.createQuery("select c from Course c " + "JOIN FETCH c.reviews " + "where c.id = :data", Course.class);
+		query.setParameter("data", id);
+		return query.getSingleResult();
+	}
 }
