ded73c7be19cbffaa9c9462f2ab21c4cd53d5653
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-22 12:14:01 -0300

    change the relationship to the Instructor to Many-To-Many

--- src/main/java/com/tidz/crudStuff/entity/Course.java
+++ src/main/java/com/tidz/crudStuff/entity/Course.java
@@ -1,11 +1,13 @@
 package com.tidz.crudStuff.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,7 +20,8 @@ public class Course {
 	private int id;
 	@Column(name = "title")
 	private String title;
-	@OneToMany
+	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
 
 	public Course() {
