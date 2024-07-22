3ba64996c17fdf9e20c4dea5c0a2fe7783f6432d
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-22 16:12:02 -0300

    add a reviews property to Course entity

--- src/main/java/com/tidz/crudStuff/entity/Course.java
+++ src/main/java/com/tidz/crudStuff/entity/Course.java
@@ -1,13 +1,18 @@
 package com.tidz.crudStuff.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +28,9 @@ public class Course {
 	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
 	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "course_id")
+	private List<Review> reviews;
 
 	public Course() {
 
@@ -56,9 +64,25 @@ public class Course {
 		this.instructor = instructor;
 	}
 
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
 	@Override
 	public String toString() {
 		return "Course [id=" + id + ", title=" + title + ", instructor=" + instructor + "]";
+	}
+
+	public void add(Review review) {
+		if (this.reviews == null) {
+			this.reviews = new ArrayList<>();
+		}
+
+		reviews.add(review);
 	}
 
 }
