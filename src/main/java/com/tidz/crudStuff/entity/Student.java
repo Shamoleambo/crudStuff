b9f9ec57310c8d8af64cc6be8739b0798e0ea04b
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-23 10:54:49 -0300

    add courses property to Student entity

--- src/main/java/com/tidz/crudStuff/entity/Student.java
+++ src/main/java/com/tidz/crudStuff/entity/Student.java
@@ -1,10 +1,18 @@
 package com.tidz.crudStuff.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +29,10 @@ public class Student {
 	private String lastName;
 	@Column(name = "email")
 	private String email;
+	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+			CascadeType.REFRESH })
+	@JoinTable(name = "course_student", joinColumns = @JoinColumn(name = "student_id"), inverseJoinColumns = @JoinColumn(name = "course_id"))
+	private List<Course> courses;
 
 	public Student() {
 
@@ -64,9 +76,25 @@ public class Student {
 		this.email = email;
 	}
 
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+
+	public void add(Course course) {
+		if (this.courses == null) {
+			this.courses = new ArrayList<>();
+		}
+
+		this.courses.add(course);
 	}
 
 }
