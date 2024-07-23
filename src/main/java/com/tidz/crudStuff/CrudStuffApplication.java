21570d1269b0393749531091739d986555fadc8c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-23 11:09:46 -0300

    implement new method findCourseAndStudentsByCourseId from AppDAO

--- src/main/java/com/tidz/crudStuff/CrudStuffApplication.java
+++ src/main/java/com/tidz/crudStuff/CrudStuffApplication.java
@@ -24,8 +24,20 @@ public class CrudStuffApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 		return runner -> {
-			createCourseAndStudent(appDAO);
+//			createCourseAndStudent(appDAO);
+			findCourseAndStudents(appDAO);
 		};
+	}
+
+	private void findCourseAndStudents(AppDAO appDAO) {
+		int id = 10;
+
+		Course course = appDAO.findCourseAndStudentsByCourseId(id);
+
+		System.out.println("Loaded course: " + course);
+		System.out.println("Students: " + course.getStudents());
+
+		System.out.println("Done!");
 	}
 
 	private void createCourseAndStudent(AppDAO appDAO) {
--- src/main/java/com/tidz/crudStuff/dao/AppDAOImpl.java
+++ src/main/java/com/tidz/crudStuff/dao/AppDAOImpl.java
@@ -115,4 +115,12 @@ public class AppDAOImpl implements AppDAO {
 		query.setParameter("data", id);
 		return query.getSingleResult();
 	}
+
+	@Override
+	public Course findCourseAndStudentsByCourseId(int id) {
+		TypedQuery<Course> query = this.entityManager
+				.createQuery("select c from Course c " + "JOIN FETCH c.students " + "where c.id = :data", Course.class);
+		query.setParameter("data", id);
+		return query.getSingleResult();
+	}
 }
